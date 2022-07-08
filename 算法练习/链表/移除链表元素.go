148091aaf3be7daac07ce6c6a2567ea7ac8a9021
飞飞帅傅 <aeiuuu@163.com>
2022-07-08 12:56:54 +0800

    移除链表元素

---
+++ 算法练习/链表/移除链表元素.go
@@ -0,0 +1,22 @@
+// author:zfy  date:2022/7/8
+
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	dummy := &ListNode{}
+	dummy.Next = head
+	cur := dummy
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummy.Next
+}
