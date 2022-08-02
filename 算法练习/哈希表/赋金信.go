5234b528ffed3a3fa21378239723096d897903f0
飞飞帅傅 <aeiuuu@163.com>
2022-08-02 23:20:50 +0800

    赋金信

---
+++ 算法练习/哈希表/赋金信.go
@@ -0,0 +1,17 @@
+// author:zfy  date:2022/8/2
+
+package main
+
+func canConstruct(ransomNote string, magazine string) bool {
+	record := make([]int, 26)
+	for _, v := range magazine {
+		record[v-'a']++
+	}
+	for _, v := range ransomNote {
+		record[v-'a']--
+		if record[v-'a'] < 0 {
+			return false
+		}
+	}
+	return true
+}
