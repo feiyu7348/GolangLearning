7e4c197ca0fc37cd58576d363728fc3d565ef8b5
飞飞帅傅 <aeiuuu@163.com>
2022-08-18 22:19:02 +0800

    移除元素

---
+++ 算法练习/双指针法/移除元素.go
@@ -0,0 +1,16 @@
+// author:zfy  date:2022/8/18
+
+package main
+
+func removeElement(nums []int, val int) int {
+	length := len(nums)
+	res := 0
+	for i := 0; i < length; i++ {
+		if nums[i] != val {
+			nums[res] = nums[i]
+			res++
+		}
+	}
+	nums = nums[:res]
+	return res
+}
