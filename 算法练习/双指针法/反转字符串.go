2ef1665c357628922b88eecac6334dfe98ed2d8c
飞飞帅傅 <aeiuuu@163.com>
2022-08-19 00:07:54 +0800

    反转字符串

---
+++ 算法练习/双指针法/反转字符串.go
@@ -0,0 +1,13 @@
+// author:zfy  date:2022/8/19
+
+package main
+
+func reverseString(s []byte) {
+	left := 0
+	right := len(s) - 1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
