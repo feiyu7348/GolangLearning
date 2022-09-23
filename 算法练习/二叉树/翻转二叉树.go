6e8c0582c33dd0eddb0a4372410d978837576d7e
飞飞帅傅 <aeiuuu@163.com>
2022-09-23 20:05:19 +0800

    翻转二叉树

--- 算法练习/二叉树/翻转二叉树.go
+++ 算法练习/二叉树/翻转二叉树.go
@@ -2,6 +2,83 @@
 
 package main
 
-var a int = 1111
-var b int = 12345
+import "container/list"
 
+
+// 前序遍历
+func invertTree11(root *TreeNode) *TreeNode {
+	if root ==nil{
+		return nil
+	}
+	root.Left,root.Right=root.Right,root.Left//交换
+
+	invertTree11(root.Left)
+	invertTree11(root.Right)
+
+	return root
+}
+
+// 后序遍历
+func invertTree12(root *TreeNode) *TreeNode {
+	stack:=[]*TreeNode{}
+	node:=root
+	for node!=nil||len(stack)>0{
+		for node!=nil{
+			node.Left,node.Right=node.Right,node.Left//交换
+			stack=append(stack,node)
+			node=node.Left
+		}
+		node=stack[len(stack)-1]
+		stack=stack[:len(stack)-1]
+		node=node.Right
+	}
+	return root
+}
+
+// 迭代前序
+func invertTree13(root *TreeNode) *TreeNode {
+	stack:=[]*TreeNode{}
+	node:=root
+	var prev *TreeNode
+	for node!=nil||len(stack)>0{
+		for node!=nil{
+			stack=append(stack,node)
+			node=node.Left
+		}
+		node=stack[len(stack)-1]
+		stack=stack[:len(stack)-1]
+		if node.Right==nil||node.Right==prev{
+			node.Left,node.Right=node.Right,node.Left//交换
+			prev=node
+			node=nil
+		}else {
+			stack=append(stack,node)
+			node=node.Right
+		}
+	}
+	return root
+}
+
+// 迭代后序
+func invertTree14(root *TreeNode) *TreeNode {
+	if root==nil{
+		return root
+	}
+	queue:=list.New()
+	node:=root
+	queue.PushBack(node)
+	for queue.Len()>0{
+		length:=queue.Len()
+		for i:=0;i<length;i++{
+			e:=queue.Remove(queue.Front()).(*TreeNode)
+			e.Left,e.Right=e.Right,e.Left//交换
+			if e.Left!=nil{
+				queue.PushBack(e.Left)
+			}
+			if e.Right!=nil{
+				queue.PushBack(e.Right)
+			}
+		}
+	}
+	return root
+}
