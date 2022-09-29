a7ba0770ca26bffa606b3bcb1bfc2b02b82baaf0
飞飞帅傅 <aeiuuu@163.com>
2022-09-29 23:32:41 +0800

    测试

---
+++ ginLearning/example/测试/setup.go
@@ -0,0 +1,19 @@
+// author:zfy  date:2022/9/29
+
+package main
+
+import "github.com/gin-gonic/gin"
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":8080")
+}
+
