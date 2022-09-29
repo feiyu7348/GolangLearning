e1c68bd2e312344bec10c31e2ac33c5e0f21573c
飞飞帅傅 <aeiuuu@163.com>
2022-09-29 23:40:44 +0800

    设置和获取cookie

---
+++ ginLearning/example/设置和获取cookie/main.go
@@ -0,0 +1,24 @@
+// author:zfy  date:2022/9/29
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/cookie", func(c *gin.Context) {
+		cookie, err := c.Cookie("gin_cookie")
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+		}
+
+		fmt.Printf("Cookie value: %s\n", cookie)
+	})
+
+	r.Run()
+}
