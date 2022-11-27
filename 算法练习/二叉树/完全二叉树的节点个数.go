aef6af184dd84ac1835b28aa4b0e75b9072762f9
飞飞帅傅 <aeiuuu@163.com>
2022-11-27 21:04:16 +0800

    完全二叉树的节点个数

--- 算法练习/二叉树/完全二叉树的节点个数.go
+++ 算法练习/二叉树/完全二叉树的节点个数.go
@@ -2,4 +2,17 @@
 
 package main
 
-var a = 1231
+// 递归
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	res := 1
+	if root.Right != nil {
+		res += countNodes(root.Right)
+	}
+	if root.Left != nil {
+		res += countNodes(root.Left)
+	}
+	return res
+}
