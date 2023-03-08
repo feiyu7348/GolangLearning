ffa924df372ed6da963e1b188a46420a936f0ff7
飞飞帅傅 <aeiuuu@163.com>
2023-03-08 17:07:41 +0800

    修改错误格式

--- Go基础语法宝典/错误类型.go
+++ Go基础语法宝典/错误类型.go
@@ -8,9 +8,7 @@ import (
 )
 
 func main() {
-	//Go内置有一个error类型，专门用来处理错误信息，Go的package里面还专门有一个包errors来处理错误
+	// Go内置有一个error类型，专门用来处理错误信息，Go的package里面还专门有一个包errors来处理错误
 	err := errors.New("emit macho dwarf: elf header corrupted")
-	if err != nil {
-		fmt.Print(err)
-	}
+	fmt.Print(err)
 }
