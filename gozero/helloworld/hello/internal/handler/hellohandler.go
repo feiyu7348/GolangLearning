e3a579cccc9431dd69bb6e5e6816ceb67def45ed
飞飞帅傅 <aeiuuu@163.com>
2023-03-26 15:20:14 +0800

    go-zero

---
+++ gozero/helloworld/hello/internal/handler/hellohandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"GolangLearning/gozero/helloworld/hello/internal/logic"
+	"GolangLearning/gozero/helloworld/hello/internal/svc"
+	"GolangLearning/gozero/helloworld/hello/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func HelloHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewHelloLogic(r.Context(), svcCtx)
+		resp, err := l.Hello(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
