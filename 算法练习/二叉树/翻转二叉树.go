b426f5500d4c9382a3606c06d88e20fb92f4519d
飞飞帅傅 <aeiuuu@163.com>
2023-03-31 15:43:00 +0800

    翻转二叉树 格式化代码

--- 算法练习/二叉树/翻转二叉树.go
+++ 算法练习/二叉树/翻转二叉树.go
@@ -4,13 +4,12 @@ package main
 
 import "container/list"
 
-
 // 前序遍历
 func invertTree11(root *TreeNode) *TreeNode {
-	if root ==nil{
+	if root == nil {
 		return nil
 	}
-	root.Left,root.Right=root.Right,root.Left//交换
+	root.Left, root.Right = root.Right, root.Left //交换
 
 	invertTree11(root.Left)
 	invertTree11(root.Right)
@@ -20,40 +19,40 @@ func invertTree11(root *TreeNode) *TreeNode {
 
 // 后序遍历
 func invertTree12(root *TreeNode) *TreeNode {
-	stack:=[]*TreeNode{}
-	node:=root
-	for node!=nil||len(stack)>0{
-		for node!=nil{
-			node.Left,node.Right=node.Right,node.Left//交换
-			stack=append(stack,node)
-			node=node.Left
+	stack := []*TreeNode{}
+	node := root
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			node.Left, node.Right = node.Right, node.Left //交换
+			stack = append(stack, node)
+			node = node.Left
 		}
-		node=stack[len(stack)-1]
-		stack=stack[:len(stack)-1]
-		node=node.Right
+		node = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		node = node.Right
 	}
 	return root
 }
 
 // 迭代前序
 func invertTree13(root *TreeNode) *TreeNode {
-	stack:=[]*TreeNode{}
-	node:=root
+	stack := []*TreeNode{}
+	node := root
 	var prev *TreeNode
-	for node!=nil||len(stack)>0{
-		for node!=nil{
-			stack=append(stack,node)
-			node=node.Left
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			stack = append(stack, node)
+			node = node.Left
 		}
-		node=stack[len(stack)-1]
-		stack=stack[:len(stack)-1]
-		if node.Right==nil||node.Right==prev{
-			node.Left,node.Right=node.Right,node.Left//交换
-			prev=node
-			node=nil
-		}else {
-			stack=append(stack,node)
-			node=node.Right
+		node = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node.Right == nil || node.Right == prev {
+			node.Left, node.Right = node.Right, node.Left //交换
+			prev = node
+			node = nil
+		} else {
+			stack = append(stack, node)
+			node = node.Right
 		}
 	}
 	return root
@@ -61,21 +60,21 @@ func invertTree13(root *TreeNode) *TreeNode {
 
 // 迭代后序
 func invertTree14(root *TreeNode) *TreeNode {
-	if root==nil{
+	if root == nil {
 		return root
 	}
-	queue:=list.New()
-	node:=root
+	queue := list.New()
+	node := root
 	queue.PushBack(node)
-	for queue.Len()>0{
-		length:=queue.Len()
-		for i:=0;i<length;i++{
-			e:=queue.Remove(queue.Front()).(*TreeNode)
-			e.Left,e.Right=e.Right,e.Left//交换
-			if e.Left!=nil{
+	for queue.Len() > 0 {
+		length := queue.Len()
+		for i := 0; i < length; i++ {
+			e := queue.Remove(queue.Front()).(*TreeNode)
+			e.Left, e.Right = e.Right, e.Left //交换
+			if e.Left != nil {
 				queue.PushBack(e.Left)
 			}
-			if e.Right!=nil{
+			if e.Right != nil {
 				queue.PushBack(e.Right)
 			}
 		}
