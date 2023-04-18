ac2d78ec607b51234b24d4b76eb2fe66255ebf28
飞飞帅傅 <aeiuuu@163.com>
2023-04-18 23:00:19 +0800

    num16

---
+++ interviewQuestion/num1-30/num16.go
@@ -0,0 +1,34 @@
+// @author:zfy
+// @date:2023/4/17 23:46
+
+package main
+
+import "fmt"
+
+type A interface {
+	ShowA() int
+}
+
+type B interface {
+	ShowB() int
+}
+
+type Work struct {
+	i int
+}
+
+func (w Work) ShowA() int {
+	return w.i + 10
+}
+
+func (w Work) ShowB() int {
+	return w.i + 20
+}
+
+func main() {
+	c := Work{3}
+	var a A = c
+	var b B = c
+	fmt.Println(a.ShowB())
+	fmt.Println(b.ShowA())
+}
