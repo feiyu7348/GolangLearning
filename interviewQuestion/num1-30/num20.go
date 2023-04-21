c1758dd7bae6ea14e3ccd53bf0324d3d90ef1f87
飞飞帅傅 <aeiuuu@163.com>
2023-04-21 22:03:08 +0800

    num20

---
+++ interviewQuestion/num1-30/num20.go
@@ -0,0 +1,31 @@
+// @author:zfy
+// @date:2023/4/21 22:02
+
+package main
+
+import "fmt"
+
+type Person struct {
+	age int
+}
+
+func main() {
+	person := &Person{28}
+
+	// 1.
+	defer fmt.Println(person.age)
+
+	// 2.
+	defer func(p *Person) {
+		fmt.Println(p.age)
+	}(person)
+
+	// 3.
+	defer func() {
+		fmt.Println(person.age)
+	}()
+
+	person = &Person{29}
+}
+
+// 29 28 28
