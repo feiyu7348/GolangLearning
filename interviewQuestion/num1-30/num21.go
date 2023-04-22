d66f1c680461a84cfdf463a42fa2566710e1d934
飞飞帅傅 <aeiuuu@163.com>
2023-04-22 23:08:42 +0800

    num21

---
+++ interviewQuestion/num1-30/num21.go
@@ -0,0 +1,22 @@
+// @author:zfy
+// @date:2023/4/22 23:08
+
+package main
+
+type S struct {
+}
+
+func f(x interface{}) {
+}
+
+func g(x *interface{}) {
+}
+
+func main() {
+	s := S{}
+	p := &s
+	f(s) //A
+	g(s) //B
+	f(p) //C
+	g(p) //D
+}
