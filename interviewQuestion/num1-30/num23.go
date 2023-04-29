a61782b2cc5f182b9e304f907899b04dfb20ed92
飞飞帅傅 <aeiuuu@163.com>
2023-04-29 20:35:13 +0800

    num23

---
+++ interviewQuestion/num1-30/num23.go
@@ -0,0 +1,26 @@
+// @author:zfy
+// @date:2023/4/29 20:33
+
+package main
+
+//func main() {
+//	s1 := []int{1, 2, 3}
+//	s2 := s1[1:]
+//	s2[1] = 4
+//	fmt.Println(s1)
+//	s2 = append(s2, 5, 6, 7)
+//	fmt.Println(s1)
+//}
+
+//[1 2 4]
+//[1 2 4]
+
+func main() {
+	if a := 1; false {
+	} else if b := 2; false {
+	} else {
+		println(a, b)
+	}
+}
+
+//1 2
