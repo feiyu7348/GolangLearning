b7d77395bdfad06ec7a0b6cb6187139c068947e3
飞飞帅傅 <aeiuuu@163.com>
2023-05-05 21:43:26 +0800

    num27

---
+++ interviewQuestion/num1-30/num27.go
@@ -0,0 +1,23 @@
+// @author:zfy
+// @date:2023/5/5 21:38
+
+package main
+
+import "fmt"
+
+type Direction int
+
+const (
+	North Direction = iota
+	East
+	South
+	West
+)
+
+func (d Direction) String() string {
+	return [...]string{"North", "East", "South", "West"}[d]
+}
+
+func main() {
+	fmt.Println(South)
+}
