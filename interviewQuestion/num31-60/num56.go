42fd4c99e2337e9eb6909a319ebfb38ebb6c435b
飞飞帅傅 <aeiuuu@163.com>
2023-08-03 22:41:49 +0800

    num56

---
+++ interviewQuestion/num31-60/num56.go
@@ -0,0 +1,18 @@
+// @author:zfy
+// @date:2023/8/3 22:40
+
+package main
+
+import "fmt"
+
+const (
+	x1 uint16 = 120
+	y1
+	s1 = "abc"
+	z1
+)
+
+func main() {
+	fmt.Printf("%T %v\n", y1, y1)
+	fmt.Printf("%T %v\n", z1, z1)
+}
