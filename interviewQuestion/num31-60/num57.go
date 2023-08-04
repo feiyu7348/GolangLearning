1f81933fa4b797c95b5ac1a80501928d56514a30
飞飞帅傅 <aeiuuu@163.com>
2023-08-04 21:09:33 +0800

    num57

---
+++ interviewQuestion/num31-60/num57.go
@@ -0,0 +1,25 @@
+// @author:zfy
+// @date:2023/8/4 21:07
+
+package main
+
+import "fmt"
+
+var o = fmt.Print
+
+func main() {
+	c := make(chan int, 1)
+	for range [3]struct{}{} {
+		select {
+		default:
+			o(1)
+		case <-c:
+			o(2)
+			c = nil
+		case c <- 1:
+			o(3)
+		}
+	}
+}
+
+//321
