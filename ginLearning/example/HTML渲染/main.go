2dd4591ea4fa676fea07c32b17f2fcfcc351594d
飞飞帅傅 <aeiuuu@163.com>
2023-08-06 15:43:34 +0800

    gin

--- ginLearning/example/HTML渲染/main.go
+++ ginLearning/example/HTML渲染/main.go
@@ -9,15 +9,17 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/")
+	r.LoadHTMLGlob("templates/*")
 	r.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
 		})
 	})
-
+	r.GET("/html", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "template.html", gin.H{"title": "Main html"})
+	})
 	err := r.Run(":8080")
 	if err != nil {
-		return 
+		return
 	}
 }
