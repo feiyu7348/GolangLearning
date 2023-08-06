89388bd1bf901713848224c987377000e5f615f2
飞飞帅傅 <aeiuuu@163.com>
2023-08-06 14:33:18 +0800

    goping

--- 28个练手项目/goping/main.go
+++ 28个练手项目/goping/main.go
@@ -15,11 +15,16 @@ import (
 )
 
 var (
-	timeOut int64
-	size    int
-	count   int
-	typ     uint8 = 8
-	code    uint8 = 8
+	timeOut      int64
+	size         int
+	count        int
+	typ          uint8 = 8
+	code         uint8 = 0
+	sendCount    int
+	successCount int
+	failCount    int
+	//minTs        int
+	//maxTs        int
 )
 
 type ICMP struct {
@@ -70,34 +75,44 @@ func main() {
 	}
 
 	defer conn.Close()
-
-	icmp := &ICMP{
-		Type:        typ,
-		Code:        code,
-		CheckSum:    0,
-		ID:          1,
-		SequenceNum: 1,
+	fmt.Printf("正在ping %s [%s] 具有 %d 字节的数据:\n", desIp, conn.RemoteAddr(), size)
+	for i := 0; i < count; i++ {
+		sendCount++
+		icmp := &ICMP{
+			Type:        typ,
+			Code:        code,
+			CheckSum:    0,
+			ID:          1,
+			SequenceNum: 1,
+		}
+		data := make([]byte, size)
+		//fmt.Println(data)
+		var buf bytes.Buffer
+		binary.Write(&buf, binary.BigEndian, icmp)
+		buf.Write(data)
+		data = buf.Bytes()
+		checkSum := checkSum(data)
+		data[2] = byte(checkSum >> 8)
+		data[3] = byte(checkSum)
+		conn.SetDeadline(time.Now().Add(time.Duration(timeOut) * time.Millisecond))
+		t1 := time.Now()
+		n, err := conn.Write(data)
+		if err != nil {
+			log.Println()
+			continue
+		}
+		buffer := make([]byte, 256)
+		n, err = conn.Read(buffer)
+		if err != nil {
+			failCount++
+			log.Println(err)
+			continue
+		}
+		successCount++
+		ts := time.Since(t1).Milliseconds()
+		fmt.Printf("来自 %d.%d.%d.%d 的回复：字节=%d,时间=%d ms,TTL=%d\n",
+			buffer[12], buffer[14], buffer[14], buffer[15], n-28, ts, buffer[8])
+		time.Sleep(time.Second)
 	}
-	data := make([]byte, size)
-	fmt.Println(data)
-	var buf bytes.Buffer
-	binary.Write(&buf, binary.BigEndian, icmp)
-	buf.Write(data)
-	data = buf.Bytes()
-	checkSum := checkSum(data)
-	data[2] = byte(checkSum >> 8)
-	data[3] = byte(checkSum)
-	conn.SetDeadline(time.Now().Add(time.Duration(timeOut) * time.Millisecond))
-	n, err := conn.Write(data)
-	if err != nil {
-		log.Println()
-		return
-	}
-	buffer := make([]byte, 65535)
-	n, err = conn.Read(buffer)
-	if err != nil {
-		log.Println()
-		return
-	}
-	fmt.Println(n)
+	fmt.Println("统计信息")
 }
