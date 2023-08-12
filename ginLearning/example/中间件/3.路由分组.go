878fba7e7023cbefc623113f057beb8ec01eda27
飞飞帅傅 <aeiuuu@163.com>
2023-08-12 13:55:11 +0800

    路由分组

--- ginLearning/example/中间件/3.路由分组.go
+++ ginLearning/example/中间件/3.路由分组.go
@@ -13,6 +13,11 @@ type UserInfo struct {
 	Age  int    `json:"age"`
 }
 
+type article struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
 type Response struct {
 	Code int    `json:"code"`
 	Data any    `json:"data"`
@@ -20,7 +25,7 @@ type Response struct {
 }
 
 func UserListView(c *gin.Context) {
-	var userList []UserInfo = []UserInfo{
+	var userList = []UserInfo{
 		{"zfy", 27},
 		{"whh", 25},
 	}
@@ -28,12 +33,35 @@ func UserListView(c *gin.Context) {
 	c.JSON(http.StatusOK, Response{0, userList, "请求成功"})
 }
 
+func ArticleListView(c *gin.Context) {
+	var articleList = []article{
+		{"go", "123"},
+		{"python", "123"},
+	}
+
+	c.JSON(http.StatusOK, Response{1, articleList, "请求成功"})
+}
+
+func UserRouter(router *gin.RouterGroup) {
+	userManager := router.Group("user_manager")
+	{
+		userManager.GET("/users", UserListView)
+	}
+}
+
+func ArticleRouter(router *gin.RouterGroup) {
+	articleManager := router.Group("user_article")
+	{
+		articleManager.GET("/articles", ArticleListView)
+	}
+}
+
 func main() {
 	router := gin.Default()
 
 	api := router.Group("/api")
-
-	api.GET("/users", UserListView)
+	UserRouter(api)
+	ArticleRouter(api)
 
 	err := router.Run(":8083")
 	if err != nil {
