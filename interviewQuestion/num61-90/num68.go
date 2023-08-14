5427fc625d3f55415c22d703fc854d7c12cac48c
飞飞帅傅 <aeiuuu@163.com>
2023-08-14 22:42:05 +0800

    num68

---
+++ interviewQuestion/num61-90/num68.go
@@ -0,0 +1,16 @@
+// @author:zfy
+// @date:2023/8/14 22:39
+
+package main
+
+func main() {
+	x := []int{0, 1, 2}
+	y := [3]*int{}
+	for i, v := range x {
+		defer func() {
+			print(v)
+		}()
+		y[i] = &v
+	}
+	print(*y[0], *y[1], *y[2])
+}
