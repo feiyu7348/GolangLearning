5ac6849e042cc80ed1589bfbcaa3f7a83104e773
飞飞帅傅 <aeiuuu@163.com>
2023-08-20 20:08:53 +0800

    配置

---
+++ go-blog/config/config.go
@@ -0,0 +1,28 @@
+// @author:zfy
+// @date:2023/8/20 20:02
+
+package config
+
+type Viewer struct {
+	Title       string
+	Description string
+	Logo        string
+	Navigation  []string
+	Bilibili    string
+	Avatar      string
+	UserName    string
+	UserDesc    string
+}
+
+type SystemConfig struct {
+	AppName         string
+	Version         float32
+	CurrentDir      string
+	CdnURL          string
+	QiniuAccessKey  string
+	QiniuSecretKey  string
+	Valine          bool
+	ValineAppid     string
+	ValineAppkey    string
+	ValineServerURL string
+}
