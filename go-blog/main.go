d5bbb9baa4dbf5ecdc80012900d082b2bae5220d
飞飞帅傅 <aeiuuu@163.com>
2023-08-20 19:51:25 +0800

    相应页面

--- go-blog/main.go
+++ go-blog/main.go
@@ -5,8 +5,10 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 type IndexData struct {
@@ -24,12 +26,25 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonStr)
 }
 
+func indexHtml(w http.ResponseWriter, r *http.Request) {
+	var indexData IndexData
+	indexData.Title = "goBlog"
+	indexData.Description = "入门"
+
+	t := template.New("index.html")
+	path, _ := os.Getwd()
+	//fmt.Println(path)
+	t, _ = t.ParseFiles(path + "/template/index.html")
+	t.Execute(w, indexData)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8085",
 	}
 
 	http.HandleFunc("/", index)
+	http.HandleFunc("/index.html", indexHtml)
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
