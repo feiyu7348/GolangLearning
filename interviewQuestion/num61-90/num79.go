9fffa1df07d51296482e2490661866908c336339
飞飞帅傅 <aeiuuu@163.com>
2023-08-25 22:24:07 +0800

    mun79

---
+++ interviewQuestion/num61-90/num79.go
@@ -0,0 +1,19 @@
+// @author:zfy
+// @date:2023/8/25 21:54
+
+package main
+
+import "fmt"
+
+type ConfigOne struct {
+	Daemon string
+}
+
+func (c *ConfigOne) String() string {
+	return fmt.Sprintf("print: %v", c)
+}
+
+func main() {
+	c := &ConfigOne{}
+	c.String()
+}
