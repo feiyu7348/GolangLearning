a7d95be6ae6101c89543a5a0a13dbdf86ace301d
飞飞帅傅 <aeiuuu@163.com>
2023-09-15 19:51:13 +0800

    mun92

---
+++ interviewQuestion/num91+/num92.go
@@ -0,0 +1,16 @@
+// @author:zfy
+// @date:2023/9/15 19:44
+
+package main
+
+import "fmt"
+
+func min(a int, b uint) {
+	var min = 0
+	min = copy(make([]struct{}, a), make([]struct{}, b))
+	fmt.Printf("The min of %d and %d is %d\n", a, b, min)
+}
+
+func main() {
+	min(1225, 256)
+}
