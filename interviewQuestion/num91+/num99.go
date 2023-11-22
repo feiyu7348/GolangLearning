8f1cb81e659f81bbd6a39799d55a0dfaf792ca13
飞飞帅傅 <aeiuuu@163.com>
2023-11-22 20:38:24 +0800

    mun99

---
+++ interviewQuestion/num91+/num99.go
@@ -0,0 +1,22 @@
+// @author:zfy
+// @date:2023/11/22 20:37
+
+package main
+
+import "fmt"
+
+func watShadowDefer(i int) (ret int) {
+	ret = i * 2
+	if ret > 10 {
+		ret := 10
+		defer func() {
+			ret = ret + 1
+		}()
+	}
+	return
+}
+
+func main() {
+	result := watShadowDefer(50)
+	fmt.Println(result)
+}
