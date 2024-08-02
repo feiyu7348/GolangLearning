4625a1cc710c986d51105ecead5dea7776a33707
飞飞帅傅 <aeiuuu@163.com>
2024-08-02 09:41:03 +0800

    翻转字符串里的单词

--- 算法练习/字符串/翻转字符串里的单词.go
+++ 算法练习/字符串/翻转字符串里的单词.go
@@ -17,32 +17,69 @@ func reverseWords(s string) string {
 		}
 		b[slowIndex] = b[fastIndex]
 		slowIndex++
-	}
+	} // 循环完最后如果有空格只会有一个空格
 	//删除尾部冗余空格
-	if slowIndex-1 > 0 && b[slowIndex-1] == ' ' {
+	if slowIndex-1 > 0 && b[slowIndex-1] == ' ' { // 此时slowIndex等于len
 		b = b[:slowIndex-1]
 	} else {
 		b = b[:slowIndex]
 	}
 	//2.反转整个字符串
-	reverse1(&b, 0, len(b)-1)
+	reverse1(b)
 	//3.反转单个单词  i单词开始位置，j单词结束位置
 	i := 0
 	for i < len(b) {
 		j := i
 		for ; j < len(b) && b[j] != ' '; j++ {
 		}
-		reverse1(&b, i, j-1)
+		reverse1(b)
 		i = j
 		i++
 	}
 	return string(b)
 }
 
-func reverse1(b *[]byte, left, right int) {
+func reverse1(b []byte) {
+	left := 0
+	right := len(b) - 1
 	for left < right {
-		(*b)[left], (*b)[right] = (*b)[right], (*b)[left]
+		b[left], b[right] = b[right], b[left]
 		left++
 		right--
 	}
+}
+
+func reverseWords1(s string) string {
+	b := []byte(s)
+
+	// 移除前面、后面、中间的空格
+	slow := 0
+	for i := 0; i < len(b); i++ {
+		if b[i] != ' ' {
+			if slow != 0 { //先理解下面的for循环，再理解这段，除了第一个单词外每个单词的前面都有一个看空格
+				b[slow] = ' '
+				slow++
+			}
+			for i < len(b) && b[i] != ' ' {
+				b[slow] = b[i]
+				slow++
+				i++
+			}
+		}
+	}
+	b = b[0:slow]
+
+	// 翻转整个字符串
+	reverse1(b)
+
+	//翻转每个单词
+	last := 0
+	for i := 0; i <= len(b); i++ {
+		if b[i] == ' ' || i == len(b) {
+			reverse1(b[last:i])
+			last = i + 1
+		}
+	}
+
+	return string(b)
 }
