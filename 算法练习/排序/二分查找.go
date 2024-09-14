d71efa98eb9303c22fa1602e5098c58295dc2f1c
zfy <aeiuuu@163.com>
2024-09-15 00:31:39 +0800

    二分查找

--- 算法练习/排序/二分查找.go
+++ 算法练习/排序/二分查找.go
@@ -18,8 +18,26 @@ func binarySearch(arr []int, target int, l int, r int) int {
 	return -1
 }
 
+func search(nums []int, target int) int {
+	n := len(nums)
+	left, right := 0, n-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
+
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7}
 	index := binarySearch(arr, 4, 0, len(arr)-1)
 	fmt.Println(index)
+	fmt.Println(search(arr, 4))
 }
