5f76ba3820a715b55e49774bdd7217731462cf00
zfy <aeiuuu@163.com>
2025-01-04 16:39:15 +0800

    单例模式

--- 设计模式/CreationalPattern/SingletonPattern.go
+++ 设计模式/CreationalPattern/SingletonPattern.go
@@ -2,4 +2,57 @@
 // author: zfy  date: 2024/12/6
 package CreationalPattern
 
-// 单例模式
+import (
+	"fmt"
+	"sync"
+)
+
+// 单例模式 https://developer.aliyun.com/article/1267689
+// 1、饿汉模式
+
+type ConfigManager struct {
+	configData map[string]string
+}
+
+var instance *ConfigManager = &ConfigManager{
+	configData: make(map[string]string),
+}
+
+func GetConfigManager() *ConfigManager {
+	return instance
+}
+
+func (cm *ConfigManager) Set(key, value string) {
+	cm.configData[key] = value
+}
+
+func (cm *ConfigManager) Get(key string) string {
+	return cm.configData[key]
+}
+
+// 2、懒汉模式
+
+type Logger struct {
+	logs []string
+}
+
+var instance1 *Logger
+var once sync.Once
+
+func GetLogger() *Logger {
+	once.Do(func() {
+		instance1 = &Logger{
+			logs: []string{}}
+	})
+	return instance1
+}
+
+func (l *Logger) AddLog(log string) {
+	l.logs = append(l.logs, log)
+}
+
+func (l *Logger) PrintLogs() {
+	for _, log := range l.logs {
+		fmt.Println(log)
+	}
+}
---
+++ 设计模式/go.mod.go
@@ -0,0 +1,3 @@
+// Package designPattern Package 设计模式
+// author: zfy  date: 2025/1/4
+package designPattern
