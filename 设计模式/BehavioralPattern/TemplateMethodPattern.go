a68dc7d6094fe2bb28dec041dc039c85625893b7
zfy <aeiuuu@163.com>
2025-01-05 12:26:10 +0800

    模板方法模式

--- 设计模式/BehavioralPattern/TemplateMethodPattern.go
+++ 设计模式/BehavioralPattern/TemplateMethodPattern.go
@@ -1,3 +1,59 @@
 // Package BehavioralPattern
 // author: zfy  date: 2024/12/6
 package BehavioralPattern
+
+import "fmt"
+
+// 模板方法模式 https://developer.aliyun.com/article/1268036
+
+// CoffeeMaker 抽象类
+type CoffeeMaker interface {
+	Prepare()
+	Brew()
+	Serve()
+}
+
+// Americano 具体类：美式咖啡
+type Americano struct {
+}
+
+func (a *Americano) Prepare() {
+	fmt.Println("准备美式咖啡的材料")
+}
+
+func (a *Americano) Brew() {
+	fmt.Println("冲泡美式咖啡")
+}
+
+func (a *Americano) Serve() {
+	fmt.Println("提供美式咖啡")
+}
+
+// Latte 具体类：拿铁咖啡
+type Latte struct {
+}
+
+func (l *Latte) Prepare() {
+	fmt.Println("准备拿铁咖啡的材料")
+}
+
+func (l *Latte) Brew() {
+	fmt.Println("冲泡拿铁咖啡")
+}
+
+func (l *Latte) Serve() {
+	fmt.Println("提供拿铁咖啡")
+}
+
+// 客户端代码
+func main() {
+	americano := &Americano{}
+	americano.Prepare()
+	americano.Brew()
+	americano.Serve()
+
+	latte := &Latte{}
+	latte.Prepare()
+	latte.Brew()
+	latte.Serve()
+}
